ec4f7032a4c9ed008e5778a3bce92799fa2ef695
Mr.Tricks <49667787+TuM4N@users.noreply.github.com>
2019-05-17 14:13:15 +0700

    intruksi.php
    Perhatikan petunjuknya dengan benar dengan benar

---
+++ config.php
@@ -0,0 +1,25 @@
+<?php
+													/*
+Lengkapi semuanya pastikan tonton dengan benar
+
+																			*/
+$walletid = "XXXXXXXX";
+$authorization = "xxxxxxxxxxxxxxxx";
+$device_id = "xxxxxxxxxxxxxxx";
+
+
+
+
+
+
+
+
+
+                      													$u = "\e[1;35m";
+																																	$c = "\e[1;36m";
+																	$g = "\e[1;32m";
+																										$r = "\e[1;31m";
+																				$p = "\e[1;37m";
+															$b = "\e[1;34m";
+														$h = "\e[1;30m";
+?>
